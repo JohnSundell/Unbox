0e9c7cf39a7c5d92ea6ef45e292ab7cdf728dce0
John Sundell <john.sundell@me.com>
2016-10-04 01:47:40 +0200

    Add note that API user shouldn’t conform to UnboxCompatible

--- Sources/Unbox.swift
+++ Sources/Unbox.swift
@@ -164,7 +164,7 @@ public protocol UnboxableWithContext {
     init(unboxer: Unboxer, context: UnboxContext) throws
 }
 
-/// Protocol that types that can be used in an unboxing process must conform to
+/// Protocol that types that can be used in an unboxing process must conform to. You don't conform to this protocol yourself.
 public protocol UnboxCompatible {
     /// Unbox a value, or either throw or return nil if unboxing couldn't be performed
     static func unbox(value: Any, allowInvalidCollectionElements: Bool) throws -> Self?
